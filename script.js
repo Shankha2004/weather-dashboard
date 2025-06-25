bc0f6623ea004e43b040877c823e71cdd266addd
Shankha <kundushankha2004@gmail.com>
2025-06-25 23:18:55 +0530

    a basic weather dashboard that shows temperature, humidity, wind speed

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const icon = document.querySelector(".fa-magnifying-glass");
+    const input = document.getElementById("searchbox");
+
+    icon.addEventListener("click", function () {
+        const city = input.value.trim();
+        if (city) {
+            getWeather(city);
+        } else {
+            alert("Please enter a city name");
+        }
+    });
+});
+
+
+function getWeather(city) {
+    const apiKey = "ada04174996b9d0b4e676738b91f322c"; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.cod === 200) {
+                document.getElementById("temp-value").textContent = data.main.temp;
+                document.getElementById("description-text").textContent = data.weather[0].description;
+                document.getElementById("wind-value").textContent = data.wind.speed;
+                document.getElementById("humidity-value").textContent = data.main.humidity;
+            } else {
+                alert("City not found!");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("Something went wrong!");
+        });
+
+        
+}
+
+
